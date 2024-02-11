3d1df1c4f234c05bd802ad2066bfcd9b90161260
Dilip <igoldlip@gmail.com>
2024-02-11 13:39:10 +0530

    init flake to manage r package and environment
    * helps to declaratively manage and reproduce the setup with required necessary tool

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "A basic flake for using R";
+  # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/f99e5f03cc0aa231ab5950a15ed02afec45ed51a";
+
+  outputs = {self, nixpkgs } :
+    let
+      system = "x86_64-linux";
+      # pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+    in {
+      devShell.${system} = pkgs.mkShell {
+
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = builtins.attrValues {
+          inherit (pkgs)
+            R
+            ;
+          inherit (pkgs.rPackages)
+            lintr
+            languageserver
+            tidyverse # includes suite of 9 packages
+            Biostrings
+            BiocManager
+            devtools
+            # limma
+            # DESeq2
+            # GEOquery
+            # umap
+            # maptools
+            # car
+            # # wateRmelon
+          # shiny
+
+          # add more packages (https://search.nixos.org)
+          # from CRAN or Bioconductor
+            ;
+        };
+
+        # Example for rstudio
+        # pkgs.rstudioWrapper.override {
+        #   packages = with pkgs.rPackages; [
+        #   dplyr
+        #     ggplot2
+        #     reshape2;
+        # ];
+        # }
+      };
+    };
+}
