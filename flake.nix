761ed207fb540f2b91172d1787ce44eda277b212
Dilip <igoldlip@gmail.com>
2024-04-15 19:18:37 +0530

    nix flake to manage the system

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A basic flake for using R";
   # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/f99e5f03cc0aa231ab5950a15ed02afec45ed51a";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/9df3e30ce24fd28c7b3e2de0d986769db5d6225d";
 
   outputs = {self, nixpkgs } :
     let
@@ -11,44 +11,37 @@
         inherit system;
       };
 
+      rpkgs = with pkgs.rPackages; [
+        lintr
+        languageserver
+        tidyverse # includes 9 packages that are down below
+        Biostrings
+        BiocManager
+        # ggmsa
+        devtools
+
+        # add more packages (https://search.nixos.org)
+        # from CRAN or Bioconductor
+      ];
+
     in {
       devShell.${system} = pkgs.mkShell {
 
-        nativeBuildInputs = [ pkgs.bashInteractive ];
-        buildInputs = builtins.attrValues {
-          inherit (pkgs)
-            R
-            bwa bowtie2 minimap2 star
-            ;
-          inherit (pkgs.rPackages)
-            lintr
-            languageserver
-            tidyverse # includes suite of 9 packages
-            Biostrings
-            BiocManager
-            devtools
-            # limma
-            # DESeq2
-            # GEOquery
-            # umap
-            # maptools
-            # car
-            # # wateRmelon
-          # shiny
+        buildInputs = with pkgs; [
+          bwa bowtie2 star minimap2
 
-          # add more packages (https://search.nixos.org)
-          # from CRAN or Bioconductor
-            ;
-        };
+          (rWrapper.override {
+            packages = [
+              rpkgs
+            ];
+          })
 
-        # Example for rstudio
-        # pkgs.rstudioWrapper.override {
-        #   packages = with pkgs.rPackages; [
-        #   dplyr
-        #     ggplot2
-        #     reshape2;
-        # ];
-        # }
+            (rstudioWrapper.override {
+              packages = [
+                rpkgs
+              ];
+            })
+        ];
       };
     };
 }
