db31230c339dd89b995c672c722295433db92511
Dilip <igoldlip@gmail.com>
2024-04-15 19:19:53 +0530

    inti: wrapper for bowtie

--- R/wrap-bowtie.R
+++ R/wrap-bowtie.R
@@ -1,10 +1,36 @@
+##' bowtie indexing from given reference genome
+##'
+##' Use combined function to make sure of bowtie arguments as options
+##'
+##' please refer 'bowtie2-build --help' for documentation.
+##' @title bowtieIndexing
+##' @param opts Options/arguments passed for bowtie index command.
+##' @param threads Threading process to be allocated, default is 4
+##' @param refgen string file path to Reference genome
+##' @param outfile string for output file name
+##' @return Indexed files and the output results for further aligning step.
+##' @export
+##' @author Dilip G
 bowtieIndex <- function(opts = "", threads = 4, refgen, outfile) {
   .checkFiles(refgen)
   args <- c(opts, "--threads", threads, refgen, outfile)
-  .execBin("echo", args)
+  .execBin("bowtie2-build", args)
 }
 
+##'  An ultrafast memory-efficient short read aligner
+##'
+##' Use combined function c() to pass in arguments as options
+##'
+##' please refer 'bowtie2 --help' for documentation/
+##' @title bowtie
+##' @param opts Options/arguments passed for bowtie index command.
+##' @param threads Threading process to be allocated, default is 4
+##' @param refgen string file path to Reference genome
+##' @param outfile string for output file name
+##' @return Aligned sam files
+##' @export
+##' @author Dilip G
 bowtie2 <- function(opts = "", threads = 4) {
   args <- c(opts, "--threads", threads)
-  .execBin("echo", args)
+  .execBin("bowtie2", args)
 }
